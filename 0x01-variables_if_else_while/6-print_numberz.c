37a33f2d46232cefa1ed5d618d3b5c9068c7c91e
AtefAbl <ablouzeabdellatif@gmail.com>
2023-02-18 01:15:36 +0100

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints single digits in base 10
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
