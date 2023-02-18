5931a2f01c148ff73977b4c3b0c451ec2d581e6c
AtefAbl <ablouzeabdellatif@gmail.com>
2023-02-18 01:10:44 +0100

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints alpha in lower and upper case.
+ * Return: always 0
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+letter = 'A';
+while (letter <= 'Z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
